1ca591d7fe34c4073fce30bd896e042468a91369
keren-sofia <84553085+keren-sofia@users.noreply.github.com>
2021-05-23 22:32:46 -0500

    Add files via upload

---
+++ Ejer. manejo tipo de datos.py
@@ -0,0 +1,22 @@
+texto = input("Escriba: Hola Mundo: ")
+print(texto)
+print(type(texto))
+lista = [(input("Escriba: 1,10,100 " ))]
+print(lista)
+print(type(lista))
+negativo = int(input("Escriba -25 : "))
+print(negativo)
+print(type(negativo))
+decimal = float(input("Escriba 1.167: "))
+print(decimal)
+print(type(decimal))
+lista2 = [(input("Escriba: Hola,Mundo "))]
+print(lista2)
+print(type(lista2))
+verdad = bool(input("Escriba True: "))
+print(verdad)
+print(type(verdad))
+uno, dos = input("Escriba Uno y dos, separados por coma: ").split(",")
+diccionario = {1:uno, 2:dos}
+print("A = ", diccionario)
+print(type(diccionario))
